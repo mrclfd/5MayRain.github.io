530434a851152d13467b08a7df3cd4160be8d78d
5MayRain <zgh2606@gmail.com>
2021-07-14 21:47:06 +0800

    Site updated: 2021-07-14 21:47:00

--- js/codeCopy.js
+++ js/codeCopy.js
@@ -2,14 +2,6 @@
 !function (e, t, a) { 
   /* code */
   var initCopyCode = function(){
-    // var copyHtml = '';
-    // var round ='';
-    // copyHtml += '<button class="btn-copy" data-clipboard-snippet="">';
-    // copyHtml += '<img src="//cdn.jsdelivr.net/gh/5MayRain/ImageHosting@latest/Blog/Website/icon/copy.svg" />';
-    // copyHtml += '</button>';
-    // round +='<div></div><div class="r2"></div><div class="r3"></div>';
-    // $(".highlight table").before(copyHtml);
-    // $("figcaption span").before(round);
     new ClipboardJS('.btn-copy', {
         target: function(trigger) {
             return trigger.nextElementSibling;
